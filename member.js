c6c458aee82bc450ed1d65af107c4938155261ca
Alejandro Jaquez <alejandrojaquez8@gmail.com>
2023-11-09 17:55:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,19 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMemberValue = skillsMember.value;
+    var skillsMemberValueLength = skillsMemberValue.length;
+    var skillsMemberValueLengthMax = 200;
+    var skillsMemberValueLengthMin = 0;
+    var skillsMemberValueLengthRemaining = skillsMemberValueLengthMax - skillsMemberValueLength;
+    var skillsMemberValueLengthRemainingText = "Remaining: " + skillsMemberValueLengthRemaining + " characters";
+    var skillsMemberValueLengthRemainingTextElement = document.getElementById("skillsMemberValueLengthRemainingText");
+    var skillsMemberValueLengthRemainingTextElementText = document.createTextNode(skillsMemberValueLengthRemainingText);
+    var skillsMemberValueLengthRemainingTextElementFirstChild = skillsMemberValueLengthRemainingTextElement.firstChild;
+    var skillsMemberValueLengthRemainingTextElementFirstChildNodeType = skillsMemberValueLengthRemainingTextElementFirstChild.nodeType;
+    var skillsMemberValueLengthRemainingTextElementFirstChildNodeTypeText = 3;
+    var skillsMemberValueLengthRemainingTextElementFirstChildNodeValue = skillsMemberValueLengthRemainingTextElementFirstChild.nodeValue;
+    var skillsMemberValueLengthRemainingTextElementFirstChildNodeValueLength = skillsMemberValueLengthRemainingTextElementFirstChildNodeValue.length;
+    var skillsMemberValueLengthRemainingTextElementFirstChildNodeValueLengthMax = 0;
+    var skillsMemberValueLengthRemainingTextElementFirstChildNodeValueLengthMin = 0
+    }
